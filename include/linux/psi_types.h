be08fd7d499185d55bf2867aee3bf000902cc7c9
Carlo Dandan <carlodandan.personal@proton.me>
2024-04-30 13:36:22 +0000

    include: wdog: define wdog_timer
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- include/linux/psi_types.h
+++ include/linux/psi_types.h
@@ -121,6 +121,10 @@ struct psi_trigger {
 	 * events to one per window
 	 */
 	u64 last_event_time;
+
+	/* Task that created the trigger */
+	char comm[TASK_COMM_LEN];
+	struct timer_list wdog_timer;
 };
 
 struct psi_group {
